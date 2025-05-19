93db78cbfbbbe83fa3d4ec8a4a9926014f9c057d
jotkma12 <jotkma12@gmail.com>
2025-05-18 21:59:24 -0500

    agrego path definido

--- panaderia/urls.py
+++ panaderia/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('finanzas/', include('finanzas.urls')),
-    path('usuarios/', include('usuarios.urls'))
-
+    path('usuarios/', include('usuarios.urls')),
+    path('', lambda request: redirect('usuarios/'))
 
 ]
